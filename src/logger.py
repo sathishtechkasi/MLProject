4d134ed51d83f8b861dfe25517feeac72dc82270
sathishtechkasi <sathishtechkasi@gmail.com>
2023-08-16 13:20:07 +0400

    Data Ingestion

--- src/logger.py
+++ src/logger.py
@@ -15,5 +15,5 @@ logging.basicConfig(
     level=logging.INFO,
     )
 
-# if __name__=="__main__":
-#     logging.info("Logging started")
+#if __name__=="__main__":
+#    logging.info("Logging started")
